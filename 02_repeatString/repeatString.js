d45a13925f270e2976a10e09fb9bd9ddbb7b7cf0
Schronding <schronding@gmail.com>
2024-05-25 15:32:31 -0600

    finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, occurences) {
+    if (occurences < 0){
+        return "ERROR";
+    }
+    let finalString = "";
+    for (let index = 0; index < occurences; index++){
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
