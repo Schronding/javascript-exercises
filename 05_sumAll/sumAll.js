fdef28f3a45d89a171f1f339828156effe879a0d
Schronding <schronding@gmail.com>
2024-05-25 19:52:56 -0600

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(small, large) {
+    if (!Number.isInteger(small) || !Number.isInteger(large)){
+        return "ERROR"; 
+    }
 
+    if (large < 0 || small < 0){
+        return "ERROR";
+    }
+    
+    if (large < small){
+        let temp = large;
+        large = small;
+        small = temp;
+    }
+
+    let result = 0;
+    while (small <= large){
+        result += small++; 
+    }
+
+    return result
 };
 
 // Do not edit below this line
