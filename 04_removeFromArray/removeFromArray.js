40ece9410595de6ff4ee0a52a993f209e611ba03
Schronding <schronding@gmail.com>
2024-05-25 19:27:12 -0600

    finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(function (value){
+        return !args.includes(value); 
+    });
 };
 
 // Do not edit below this line
