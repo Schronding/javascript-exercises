1d71124eac70d18fc6f0fec2f3a421c41fd35c9d
Schronding <schronding@gmail.com>
2024-05-27 15:12:55 -0600

    finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFarenheit) {
+  let tempCelcius = (tempFarenheit - 32) / 1.8;
+  tempCelcius = Math.ceil(tempCelcius * 10) 
+  tempCelcius /= 10; 
+  return tempCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+  let tempFarenheit = (tempCelcius * 1.8) + 32;
+  tempFarenheit = Math.ceil(tempFarenheit * 10) 
+  tempFarenheit /= 10; 
+  return tempFarenheit;
 };
 
 // Do not edit below this line
